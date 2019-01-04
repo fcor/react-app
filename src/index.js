cf81154f89d62e6af3aaeefe29c195c5afa20b5f
Fabio Cortés <fcor14@hotmail.com>
2019-01-04 17:08:06 -0500

    Prettier support added

--- src/index.js
+++ src/index.js
@@ -2,22 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles.scss';
 
-const App = () => 
+const App = () => (
   <div className="App">
     <header className="App-header">
-      <img src='../dist/images/logo.svg' className="App-logo" alt="logo" />
+      <img src="../dist/images/logo.svg" className="App-logo" alt="logo" />
       <p>
-        Welcome to this React boilerplate. <br/> Edit <code>src/App.js</code> and save to reload.
+        Welcome to this React boilerplate. <br /> Edit <code>src/App.js</code> and save to reload.
       </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
         React Docs
       </a>
     </header>
   </div>
+);
 
 ReactDOM.render(<App />, document.getElementById('app'));
